4e9f378b698416817a3562607da8dd5f7f8941d7
Ali Bouguerra <alibouguerra38@gmail.com>
2026-01-20 15:22:07 +0100

    Initialize tsParticles configuration in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+tsParticles.load("tsparticles", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#4fa3ff" },
+    links: {
+      enable: true,
+      color: "#4fa3ff",
+      distance: 150
+    },
+    move: {
+      enable: true,
+      speed: 1
+    },
+    size: { value: 2 }
+  }
+});
